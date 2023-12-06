fa1c70eb135f4331570a766184458aad18b9e9ff
Marcos Vassoler <marcosvassoler@finchsolucoes.com.br>
2023-12-06 14:55:06 -0300

    Commit

---
+++ src/main/java/com/mvassoler/runner/runner/config/WebConfig.java
@@ -0,0 +1,9 @@
+package com.mvassoler.runner.runner.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+}
